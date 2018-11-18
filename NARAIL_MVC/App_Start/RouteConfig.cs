e2f900e884b7e5aaa5f4428a6e4da6ec78e8f2c9
Ali KAVAK <Ali KAVAK@DESKTOP-LD47CL2>
2018-11-18 22:03:01 +0300

    Blog ve iletisim alanları Eklendi. Görselliklete bir kaç değişiklik yapıldı

--- NARAIL_MVC/App_Start/RouteConfig.cs
+++ NARAIL_MVC/App_Start/RouteConfig.cs
@@ -12,7 +12,21 @@ namespace NARAIL_MVC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                name: "About",
+                url: "hakkimizda",
+                defaults: new { controller = "Home", action = "About" }
+            );
+            routes.MapRoute(
+                name: "Blog",
+                url: "blog",
+                defaults: new { controller = "Home", action = "Blog" }
+            );
+                routes.MapRoute(
+                 name: "Contact",
+                 url: "iletisim",
+                 defaults: new { controller = "Home", action = "Contact" }
+             );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
